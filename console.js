7709068d1135770d3e17874792d3cf87521bd7e8
Sheng-Te Tsao <tsaost@gmail.com>
2022-10-18 22:25:06 -0400

    Rename getConsolePrintList to getConsolePrints.  Add getDebugLevelSelectHtml(selectClass).  Add new parameter duplicate to getConsolePrints() so that the code can set a duplicate text to console.debug().

--- console.js
+++ console.js
@@ -1,7 +1,7 @@
 'use strict'
 
 let ErrorHandler = function(text) {
-	// This will be redefined when getConsolePrintList is called later
+	// This will be redefined when getConsolePrints is called later
 	// once options.dbgLevel is know
 	return error => {
 		console.trace();
@@ -16,22 +16,49 @@ const consoleClearErrorAlert = function(text) {
 	alert(text);
 }
 
-function getConsolePrintList(prefix, level) {
-	const debug = level > 4? text => console.debug(prefix + text) : _ => {};
-	const info = level > 3? text => console.info(prefix + text) : _ => {};
-	const log = level > 2? text => console.log(prefix + text) : _ => {};
-	const warn = level > 1? text => console.warn(prefix + text) : _ => {};
+
+function getDebugLevelSelectHtml(selectClass) {
+	return 	'<select class="' + selectClass + '" name="debugLevel">' +
+	    '<option value="0">0. None</option>' +
+	    '<option value="1">1. Error</option>' +
+	    '<option value="2">2. Warning</option>' +
+	    '<option value="3">3. Log</option>' +
+	    '<option value="4">4. Info</option>' +
+	    '<option value="5">5. Debug</option>' +
+		'</select>';
+}
+
+
+function getConsolePrints(prefix, level, duplicate) {
+
+	function Dup(prefix, consoleX, consolePrefix, duplicate) {
+		return duplicate? text => {
+			text = prefix + text
+			consoleX(consolePrefix + prefix + text);
+			// Send a duplicate copy to console.debug(),
+			// so that one can see everything with debug alone.
+			// One then restrict to just one category by unchecking debug
+			// in thye Web/Browser Console and then check the category to see.
+			// This is easier than having to unclick ALL other categories.
+			console.debug(prefix + text);
+		} : text => consoleX(prefix + text);
+	}
+
+	const debug = level>4? text => console.debug(prefix + text)  : _ => {};
+	const info  = level>3? Dup(prefix, console.info, 'F ', duplicate) : _ => {};
+	const log   = level>2? Dup(prefix, console.log,  'L ', duplicate) : _ => {};
+	const warn  = level>1? Dup(prefix, console.warn, '',   duplicate) : _ => {};
 	const errorAlert = level > 0?
-		text => consoleClearErrorAlert(prefix + text) : _ => console.trace();
+		Dup(prefix, consoleClearErrorAlert, duplicate): _ => console.trace();
 
 	// Redefine ErrorHandler now that we know options.dbgLevel
 	ErrorHandler = function(text) {
 		return error => {
-			const errorText = text + ': ' + error;
+			const errorText = prefix + text + ': ' + error;
 			if (errorText.indexOf("Could not establish connection. " +
 								  "Receiving end does not exist.") > 0) {
 				// Not really an error so just make a note of it.
-				console.debug(errorText);
+				if (level > 4) console.debug(errorText);
 			} else {
 				console.trace();
 				console.error(errorText);
